b4bd65acdbc995520a1402437f8fb214ea24fa78
LunaDiegoLeo <diegolhl678@gmail.com>
2026-07-29 00:17:06 -0600

    fix: Set default DNS result order to prioritize IPv4

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
 import helmet from 'helmet';
 import express from "express";
 import cors from "cors";
