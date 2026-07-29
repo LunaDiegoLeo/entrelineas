383a7dd5c87bb1aa45c28915863db13b6fcea623
LunaDiegoLeo <diegolhl678@gmail.com>
2026-07-29 00:09:25 -0600

    fix: Update SMTP configuration to use port 587 and set secure to false

--- backend/routes/usuarios.route.js
+++ backend/routes/usuarios.route.js
@@ -14,8 +14,8 @@ const registroLimiter = rateLimit({
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com", // Le decimos exactamente a dónde ir
-    port: 465, // Usamos el puerto seguro de Google
-    secure: true,
+    port: 587, // Usamos el puerto seguro de Google
+    secure: false,
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS
